a378a3f64da18d51ef8819a4d208937e9cf25b7e
dityath <dityaathallah107@gmail.com>
2022-06-19 19:38:53 +0700

    tampilan beres agnsa

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Text } from "@chakra-ui/react";
+import { Box, Button, Heading, VStack, Text } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Link from "next/link";
 
@@ -11,11 +11,21 @@ const Home: NextPage = () => {
       justifyContent={"center"}
       alignItems={"center"}
     >
-      <Link href={"/upload-bills"}>
-        <Button colorScheme={"purple"} size={"sm"}>
-          Split My Bill
-        </Button>
-      </Link>
+      <VStack textAlign={"center"} gap={5}>
+        <Heading color={"#211C70"}>
+          Solusi cepat <br />
+          <Text as={"span"} color={"#DB4D4D"}>
+            patungan
+          </Text>
+          <br />
+          tongkrongan anda
+        </Heading>
+        <Link href={"/upload-bills"}>
+          <Button colorScheme={"purple"} size={"lg"}>
+            Split My Bill
+          </Button>
+        </Link>
+      </VStack>
     </Box>
   );
 };
